e38f95c7b8ede7572fc36f757b54f9144990f856
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-08-14 20:01:01 +0200

    Create NumberOfEvents Component
    Add test case

---
+++ src/components/NumberOfEvents.js
@@ -0,0 +1,33 @@
+import { useState } from "react"
+
+
+const NumberOfEvents = ({ events }) => {
+  const [eventCount, setEventCount] = useState(32); // default to 32 events
+
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setEventCount(value)
+};
+
+   const filteredEvents = events.slice(0, Math.min(eventCount, events.length));
+
+  return (
+    <div id="numberOfEvents">
+      <input
+      type="text"
+      className="event-numbers"
+      placeholder="events"
+      value={eventCount}
+      onChange={handleInputChange}
+      aria-label="Number of events to display"
+      />
+      <ul>
+        {filteredEvents.map((event, index) => (
+          <li key={index}>{event.summary}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default NumberOfEvents;
