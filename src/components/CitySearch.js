717925b27bc2bf599e26176d0e05d16be5581fb3
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-08-15 15:24:17 +0200

    Update CitySearch.js

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,16 +1,20 @@
-import { useState } from "react"
+import { useState, useEffect } from "react";
 
-const CitySearch = ({ allLocations }) => {
+const CitySearch = ({ allLocations, setCurrentCity }) => {
   const [showSuggestions, setShowSuggestions] = useState(false);
   const [query, setQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    setSuggestions(allLocations);
+  }, [`${allLocations}`]);
+
 
   const handleInputChanged = (event) => {
     const value = event.target.value;
     const filteredLocations = allLocations ? allLocations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     }) : [];
-
     setQuery(value);
     setSuggestions(filteredLocations);
   };
@@ -18,7 +22,8 @@ const CitySearch = ({ allLocations }) => {
   const handleItemClicked = (event) => {
     const value = event.target.textContent;
     setQuery(value);
-    setShowSuggestions(false); // to hide the list
+    setShowSuggestions(false);
+    setCurrentCity(value);
   };
 
   return (
@@ -46,4 +51,4 @@ const CitySearch = ({ allLocations }) => {
   )
 }
 
-export default CitySearch
+export default CitySearch;
