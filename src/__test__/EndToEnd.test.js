437f1139b45466e6753c9b1e3748653bfab14a51
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-08-20 15:00:52 +0200

    Added End-to-End testing for feature
    Added testing for feature: "filter events by city"

--- src/__test__/EndToEnd.test.js
+++ src/__test__/EndToEnd.test.js
@@ -38,3 +38,53 @@ describe('show/hide an event details', () => {
   });
 
 });
+
+
+describe('filter events by city', () => {
+
+  let browser;
+  let page;
+  beforeAll(async () => {
+    browser = await puppeteer.launch(/* 
+        {
+            headless: false,
+            slowMo: 250, // slow down by 250ms,
+            timeout: 0 // removes any puppeteer/browser timeout limitations (this isn't the same as the timeout of jest)
+          } );*/ )
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
+
+  test('User can see all events upon opening the app', async () => {
+    const eventItems = await page.$$('.event');
+  expect(eventItems.length).toBeGreaterThan(0); 
+  });
+
+  test('User receive a list of cities (suggestions) while typing a city in the city textbox', async () => {
+    await page.type('.city', 'Berlin')
+
+    const suggestions = await page.$$eval('.suggestions li', items => items.map(item => item.textContent));
+
+    expect(suggestions).toContain('Berlin, Germany');
+    expect(suggestions).toContain('See all cities');
+
+  });
+
+  test('User can select a specific city from the suggested list and receive a list of upcoming events of that city', async () => {
+    await page.type('.city', 'Berlin');
+    await page.click('.suggestions li:nth-child(1)'); // Select the first suggestion (Berlin, Germany)
+  
+    const eventItems = await page.$$('.event');
+    
+    eventItems.forEach(async (item) => {
+      const location = await item.$eval('p:nth-of-type(2)', el => el.textContent);
+      expect(location).toContain('Berlin, Germany'); // Ensures events are from Berlin
+    });
+
+  });
+
+});
