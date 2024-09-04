0a439ed37588056a6a6cdbfdc39c56fa6a877738
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-09-04 09:55:03 +0200

    Added PieChart
    Added Recharts Piechart and modified accordingly

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import EventList from "./components/EventList"
 import CitySearch from "./components/CitySearch"
 import NumberOfEvents from "./components/NumberOfEvents"
 import CityEventsChart from "./components/CityEventsChart"
+import EventGenresChart from "./components/EventGenresChart"
 import { InfoAlert, ErrorAlert, WarningAlert } from "./components/Alert"
 import "./App.css"
 
@@ -61,10 +62,13 @@ const App = () => {
         setCurrentNOE={setCurrentNOE}
         setErrorAlert={setErrorAlert}
       />
-      <CityEventsChart
-        allLocations={allLocations}
-        events={events}
-      />
+      <div className="charts-container"> 
+        <CityEventsChart
+          allLocations={allLocations}
+          events={events}
+        />
+        <EventGenresChart events={events} />
+      </div>
       <EventList events={events} />
     </div>
   )
