41a47962a0c6bdbcfc97853a1ab5a358b38d7820
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-09-10 09:20:26 +0200

    Added allLocations variable to useEffect

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -7,7 +7,7 @@ const CityEventsChart = ({ allLocations, events }) => {
 
     useEffect(() => {
         setData(getData());
-    }, [events]);
+    }, [events, allLocations]);
 
 
     const getData = () => {
